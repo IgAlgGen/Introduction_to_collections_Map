96bb18c065e953730c72f92a73fe653d137aa77b
Igalgen <genvarskiy.i.a@gmail.com>
2023-02-08 21:46:59 +0400

    Выполнил домашнее задание.
    Надеюсь, я правильно понял условие: "В консоль выводится информация об автомобилях без повтора. В мапе в качестве ключей используется другой объект, который является полным аналогом уже существующего объекта."

--- src/Solution.java
+++ src/Solution.java
@@ -5,6 +5,7 @@ import Maintenance.Mechanic;
 import Maintenance.ServiceStation;
 import Transport.*;
 
+import java.sql.Driver;
 import java.util.*;
 
 
@@ -75,26 +76,36 @@ public class Solution {
         for (int i = 0; i < 4; i++) {
             serviceStation.doService();
         }
+        Map<String, Mechanic> transportMechanicHashMap = new HashMap<>();
+        transportMechanicHashMap.put(String.valueOf(transportList.get(0)), a1);
+        transportMechanicHashMap.put(String.valueOf(transportList.get(1)), a2);
+        transportMechanicHashMap.put(String.valueOf(transportList.get(2)), a3);
 
+        System.out.println();
+
+        for (Map.Entry<String, Mechanic> objectObjectEntry: transportMechanicHashMap.entrySet()) {
+            System.out.println(objectObjectEntry.getKey() + " - " + objectObjectEntry.getValue());
+        }
+        // Надеюсь, я правильно понял условие: "В консоль выводится информация об автомобилях без повтора.
+        // В мапе в качестве ключей используется другой объект, который является полным аналогом уже существующего объекта."
     }
 
-   public static void printSolution(Transport<?> transport) {
-       System.out.println("Водитель " + transport.getDriverInfo() + ", управляет автомобилем " + transport + " и будет участвовать в заезде.");
-   }
+    public static void printSolution(Transport<?> transport) {
+        System.out.println("Водитель " + transport.getDriverInfo() + ", управляет автомобилем " + transport + " и будет участвовать в заезде.");
+    }
 
-   public static void passingDiagnostics(Transport<?> transport) {
+    public static void passingDiagnostics(Transport<?> transport) {
 
-       try {
-           if (transport.isNeedDiagnostics()) {
-               System.out.println("Транспортное средство " +transport + " прошло диагностику.");
-           } else {
-               System.out.println("Транспортное средство " + transport + " не опознано.");
-           }
-       } catch (TransportTypeException e) {
-           System.out.println("Автобусы не должны проходить диагностику.");
-       }
-   }
-
+        try {
+            if (transport.isNeedDiagnostics()) {
+                System.out.println("Транспортное средство " + transport + " прошло диагностику.");
+            } else {
+                System.out.println("Транспортное средство " + transport + " не опознано.");
+            }
+        } catch (TransportTypeException e) {
+            System.out.println("Автобусы не должны проходить диагностику.");
+        }
+    }
 
 
 }
